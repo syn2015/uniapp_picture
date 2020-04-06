4159e7c435488fca57852a5f96350d790349a70a
431059 <syn201012@163.com>
2020-04-06 16:46:28 +0800

    src/utils/ src/uni.scss src/app.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  onLaunch: function() {},
+  onShow: function() {},
+  onHide: function() {},
+  methods: {
+    noDataToast() {
+      uni.showToast({
+        title: "没有数据了",
+        icon: "none"
+      });
+    }
+  }
+};
+</script>
+
+<style>
+@import "./styles/iconfont.wxss";
+@import "./styles/base.wxss";
+</style>
